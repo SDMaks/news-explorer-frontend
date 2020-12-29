daa9758887357d122a668f3a18416b69b3bfb6d0
SDMaks <nwctim@yandex.ru>
2020-12-29 15:21:12 +0300

    поменял ссылку на апи

--- src/js/configfile/configfile.js
+++ src/js/configfile/configfile.js
@@ -1,7 +1,7 @@
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const SERVER_URL = NODE_ENV === 'development'
   ? 'http://localhost:3200'
-  : 'https://www.api.sdnews.students.nomoreparties.space';
+  : 'https://www.api.emnews.students.nomoreparties.space';
 
 export const configurl = {
   url: `${SERVER_URL}`,
